2237eeaa61afec409774d04bf11ce033cae041d2
Ubuntu <230340017+kuruton2007-droid@users.noreply.github.com>
2025-10-25 06:40:13 +0000

    1025

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,36 @@
 class BooksController < ApplicationController
-  def new
+  def new 
+    @book = Book.new 
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] ="Book was successfully created."
+      redirect_to book_path(@book)
+      
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
   def index
+    @bookall = Book.all
+    @book  = Book.new
+    
+    
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+private
+
+def book_params
+   params.require(:book).permit(:title, :body)
   end
 end
