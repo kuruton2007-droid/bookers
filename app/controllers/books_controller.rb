aea4171cb4abb9bdf91f958fd3d8047440e24402
Ubuntu <230340017+kuruton2007-droid@users.noreply.github.com>
2025-10-25 13:25:58 +0000

    perfect

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,14 +8,14 @@ class BooksController < ApplicationController
     if @book.save
       flash[:notice] ="Book was successfully created."
       redirect_to book_path(@book)
-      
     else
-      render :new, status: :unprocessable_entity
+      @books = Book.all
+      render :index, status: :unprocessable_entity
     end
   end
   
   def index
-    @bookall = Book.all
+    @books = Book.all
     @book  = Book.new
     
     
@@ -30,13 +30,19 @@ class BooksController < ApplicationController
   end
 
 def update
-  book = Book.find(params[:id])
-  if book.update(book_params)
+  @book = Book.find(params[:id])
+  if @book.update(book_params)
     flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book)
   else
-    render :edit
+    render :edit, status: :unprocessable_entity
   end
+end
+
+def destroy
+  @book = Book.find(params[:id])
+  @book.destroy
+  redirect_to books_path, notice: "Book was successfully destroyed."
 end
 
 private
