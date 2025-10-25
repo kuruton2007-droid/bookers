f29a81d9800d2ae4f4324c24bd76198e8504044b
Ubuntu <230340017+kuruton2007-droid@users.noreply.github.com>
2025-10-25 08:36:32 +0000

    edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,18 @@ class BooksController < ApplicationController
   end
 
   def edit
+     @book = Book.find(params[:id])
   end
+
+def update
+  book = Book.find(params[:id])
+  if book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
+  else
+    render :edit
+  end
+end
 
 private
 
